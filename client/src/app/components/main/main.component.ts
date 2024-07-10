a71f0cb7e62ea00ee3d551a13287ac87a78290eb
Antoine-NVE <antoinenavette@gmail.com>
2024-07-10 11:21:38 +0200

    Formulaire d'ajout de tâche

--- client/src/app/components/main/main.component.ts
+++ client/src/app/components/main/main.component.ts
@@ -1,23 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { TaskCardComponent } from '../task-card/task-card.component';
 import { CommonModule } from '@angular/common';
-import { Task } from '../../models/task.model';
+import { NewTask, Task } from '../../models/task.model';
 import { TaskService } from '../../services/task.service';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-main',
     standalone: true,
-    imports: [TaskCardComponent, CommonModule],
+    imports: [TaskCardComponent, CommonModule, ReactiveFormsModule],
     templateUrl: './main.component.html',
     styleUrl: './main.component.css',
 })
 export class MainComponent implements OnInit {
+    @ViewChild('taskInput')
+    public taskInput!: ElementRef<HTMLInputElement>;
+
     public tasksDone: Task[] = [];
     public taskNotDone: Task[] = [];
 
-    constructor(private taskService: TaskService) {}
+    public isSubmitted: boolean = false;
+    public isLoading: boolean = false;
+
+    public addingNewTask: boolean = false; // Affichage du formulaire d'ajout
+    public taskForm!: FormGroup;
+
+    constructor(private taskService: TaskService, private cdRef: ChangeDetectorRef) {}
 
     ngOnInit(): void {
+        this.taskForm = new FormGroup({
+            task: new FormControl('', Validators.required),
+        });
+
         this.taskService.readAll().subscribe({
             next: (tasks) => {
                 // On sépare les tâches effectuées de celles en cours
@@ -28,6 +42,48 @@ export class MainComponent implements OnInit {
         });
     }
 
+    // Affichage du formulaire
+    public openForm() {
+        this.addingNewTask = true;
+
+        // On demande à détecter les changements car sinon le focus se fait sur un élément pas encore existant
+        // Cela marcherait avec un setTimeout()
+        this.cdRef.detectChanges();
+        this.taskInput.nativeElement.focus(); // On met le focus sur l'input
+    }
+
+    // Fermeture du formulaire
+    public closeForm() {
+        this.addingNewTask = false;
+        this.isSubmitted = false;
+        this.taskForm.reset();
+    }
+
+    public taskFormSubmit() {
+        this.isSubmitted = true;
+
+        if (!this.taskForm.get('task')!.errors) {
+            this.isLoading = true;
+
+            const task: NewTask = {
+                task: this.taskForm.get('task')!.value,
+                isDone: false,
+            };
+
+            this.taskService.create(task).subscribe({
+                next: (task) => {
+                    this.isLoading = false;
+
+                    // On ferme le formulaire et on ajoute la nouvelle tâche à notre array
+                    this.closeForm();
+                    this.taskNotDone.push(task);
+                },
+                error: (error) => (this.isLoading = false),
+            });
+        }
+    }
+
+    // Permet de récupérer les tâches des composants enfants
     public handleTask(updatedTask: Task) {
         // On vient déplacer la tâche d'un array à l'autre
         if (updatedTask.isDone) {
